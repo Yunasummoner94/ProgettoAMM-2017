d11fc3dc10819c0baa137a113764dc8071fce7b8
Yunasummoner <saravaleriascardigli@yahoo.it>
2017-06-21 16:35:29 +0200

    Login

--- src/java/amm/nerdbook/Login.java
+++ src/java/amm/nerdbook/Login.java
@@ -5,10 +5,109 @@
  */
 package amm.nerdbook;
 
+import amm.nerdbook.Classi.UtenteFactory;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author sarav
  */
-public class Login {
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
     
+    protected void processRequest(HttpServletRequest request, 
+                                  HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        //apre la sessione
+        HttpSession session = request.getSession();
+        
+        //distrugge la sessione
+        if(request.getParameter("logout") != null) {
+            session.invalidate();
+            request.getRequestDispatcher("loginForm.jsp").forward(request, response);
+            return;
+        }
+        
+        //se l'utente è già loggato
+        if (session.getAttribute("loggedIn") != null &&
+            session.getAttribute("loggedIn").equals(true)) {
+         
+            request.getRequestDispatcher("Bacheca").forward(request,response);
+            return;
+        
+        //se l'utente non è loggato
+        } else {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            
+            if (username != null && password != null) {
+                int loggedUserID = UtenteFactory.getInstance().getIdByUserAndPassword(username, password);
+                
+                //se l'utente è valido
+                if (loggedUserID != -1) {
+                    session.setAttribute("loggedIn", true);
+                    session.setAttribute("loggedUserID", loggedUserID);
+                    
+                    request.getRequestDispatcher("Bacheca").forward(request,response);
+                    return;
+                }
+                //se la combinazione non è valida
+                  else {
+                    request.setAttribute("invalidData", true);
+                    request.getRequestDispatcher("Login.jsp").forward(request, response);
+                    return;
+               }
+            }
+            request.getRequestDispatcher("Login.jsp").forward(request,response);
+        }
+        
+    }
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
